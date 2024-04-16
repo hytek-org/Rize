234e1bf54ae25e7e4aaf2d8b0955c26f69b643bf
kuldeep <hytek112@gmail.com>
2024-04-16 19:46:00 +0530

    add login

---
+++ app/src/main/java/com/hytek/rize/GuestActivity.java
@@ -0,0 +1,73 @@
+package com.hytek.rize;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.hytek.rize.auth.LoginActivity;
+import com.hytek.rize.auth.RegisterActivity;
+
+public class GuestActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_guest);
+
+        Button log_in = findViewById(R.id.log_in);
+        Button register_button = findViewById(R.id.register_button);
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity here
+                Intent intent = new Intent(GuestActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the new activity here
+                Intent intent = new Intent(GuestActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button google_register_button = findViewById(R.id.google_button);
+        google_register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve the web client ID from the environment variable
+                String webClientId = BuildConfig.WEB_CLIENT_ID;
+
+                // Check if the web client ID is not null
+                if (webClientId != null) {
+                    // Convert the web client ID to lowercase and display it in a Toast
+                    Toast.makeText(GuestActivity.this, webClientId.toLowerCase(), Toast.LENGTH_LONG).show();
+                } else {
+                    // If the web client ID is null, display an error message
+                    Toast.makeText(GuestActivity.this, "WEB_CLIENT_ID is not defined", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+
+    }
+
+}
+
+
+
+
+
+
---
+++ app/src/main/java/com/hytek/rize/auth/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.hytek.rize.auth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hytek.rize.HomeActivity;
+import com.hytek.rize.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextInputLayout emailTextInputLayout;
+    private TextInputLayout passwordTextInputLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailTextInputLayout = findViewById(R.id.email_field);
+        passwordTextInputLayout = findViewById(R.id.password_field);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+        Button registerButton = findViewById(R.id.register_link);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+
+    private void loginUser() {
+        String email = emailTextInputLayout.getEditText().getText().toString().trim();
+        String password = passwordTextInputLayout.getEditText().getText().toString().trim();
+
+        // Input validation
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Invalid email format", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Attempt to sign in the user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        checkEmailVerification();
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(LoginActivity.this, "Login failed: " + task.getException().getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    // Check if user's email is verified
+    private void checkEmailVerification() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            user.reload().addOnCompleteListener(task -> {
+                if (user.isEmailVerified()) {
+                    // Email is verified, proceed to login activity
+                    Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                    //  Navigate to next activity or handle successful registration
+                    // Start the new activity here
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    // Finish the current activity
+                    finish();
+
+                } else {
+                    // Email is not verified, inform the user
+                    Toast.makeText(LoginActivity.this, "Please verify your email before logging in.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+}
