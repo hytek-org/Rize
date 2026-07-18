72749e7ef754ab163768e5ee695ed55ed92d33e5
kuldeepsharma1 <hytek112@gmail.com>
2026-07-18 13:10:35 +0530

    Add Google authentication implementation along with RevenueCat and Firebase dependency installation

--- src/components/animated-icon.tsx
+++ src/components/animated-icon.tsx
@@ -99,7 +99,7 @@ export function AnimatedIcon() {
   return (
     <View style={styles.iconContainer}>
       <Animated.View entering={glowKeyframe.duration(60 * 1000 * 4)} style={styles.glow}>
-        <Image style={styles.glow} source={require('@/assets/images/logo-glow.png')} />
+        <Image style={styles.glow} source={require('@/assets/images/android-icon-background.png')} />
       </Animated.View>
 
       <Animated.View entering={keyframe.duration(DURATION)} style={styles.background} />
